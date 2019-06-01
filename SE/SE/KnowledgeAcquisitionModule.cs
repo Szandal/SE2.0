455f9cdf487fb0934802a06ad3ceefe5046df9dc
Łukasz <luki356@hotmail.com>
2019-06-01 13:23:15 +0200

    1.2

---
+++ SE/SE/KnowledgeAcquisitionModule.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SE
+{
+    class KnowledgeAcquisitionModule
+    {
+
+        private void AddRule()
+        {
+
+        }
+        private void AddFact()
+        {
+
+        }
+
+        private void RemoveRule()
+        {
+
+        }
+        private void RemoveFact()
+        {
+
+        }
+
+        private bool ChecRule()
+        {
+            return false;
+        }
+        private bool CheckFact()
+        {
+            return false;
+        }
+
+
+    }
+}
--- SE/SE/RuleStrategy.cs
+++ SE/SE/RuleStrategy.cs
@@ -8,5 +8,30 @@ namespace SE
 {
     class RulesStrategy
     {
+        public Rule GetRule(string Strategy, List<Rule> ListOfRules)
+        {
+            switch (Strategy)
+            {
+                case "1":
+                    return null;
+                    break;
+                case "2":
+                    return null;
+                    break;
+                default:
+                    throw new Exception();
+            }
+        }
+
+        private Rule Strategia1()
+        {
+            return null;
+        }
+
+        private Rule Strategia2()
+        {
+            return null;
+        }
+
     }
 }
